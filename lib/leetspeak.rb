377d06659755b0d2bc25cfbdc99fc090c92193b3
Seve Sheldon <sevesheldon@gmail.com>
2017-03-14 16:20:20 -0700

    modify previous specs to include uppercase letters and add a fourth spec (which is only a capital letter).

--- lib/leetspeak.rb
+++ lib/leetspeak.rb
@@ -3,10 +3,12 @@ class String
     new_word = []
     split_word = self.split("")
     split_word.each() do |letter|
-      if letter == "e"
+      if letter == "e" || letter == "E"
         new_word.push("3")
-      elsif letter == "o"
-        new_word.push("0") 
+      elsif letter == "o" || letter == "O"
+        new_word.push("0")
+      elsif letter == "I"
+        new_word.push("1")    
       else
         new_word.push(letter)
       end
--- spec/leetspeak_spec.rb
+++ spec/leetspeak_spec.rb
@@ -8,10 +8,14 @@ describe('String#leetspeak') do
   end
 
   it('replaces the letter "e" with "3"') do
-    expect(("eve").leetspeak()).to(eq("3v3"))
+    expect(("Eve").leetspeak()).to(eq("3v3"))
   end
 
   it('replaces the letter "o" with "0"') do
-    expect(("room").leetspeak()).to(eq("r00m"))
+    expect(("Opossum").leetspeak()).to(eq("0p0ssum"))
+  end
+
+  it('replaces the letter "I" (but not "i") with "1"') do
+    expect(("Init").leetspeak()).to(eq("1nit"))
   end
 end
