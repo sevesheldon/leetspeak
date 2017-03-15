cc8a78a41d0454c9aa537b6641eca64304154de0
Seve Sheldon <sevesheldon@gmail.com>
2017-03-14 17:08:21 -0700

    add the fifth and sixth working specs.

--- lib/leetspeak.rb
+++ lib/leetspeak.rb
@@ -1,6 +1,7 @@
 class String
   define_method(:leetspeak) do
     new_word = []
+    last_letter = " "
     split_word = self.split("")
     split_word.each() do |letter|
       if letter == "e" || letter == "E"
@@ -8,10 +9,17 @@ class String
       elsif letter == "o" || letter == "O"
         new_word.push("0")
       elsif letter == "I"
-        new_word.push("1")    
+        new_word.push("1")
+      elsif letter == "s"
+        if last_letter == " " # As it iterates through, if the last letter thru the each loop is a " " (which is what separates words (it is also declared as " " at the beginning so we start that way)), don't change the first letter.
+          new_word.push(letter)
+        else
+          new_word.push("z")
+        end
       else
         new_word.push(letter)
       end
+    last_letter = letter # Changes everytime through the loop to be the "last letter" ran thru the loop
     end
     new_word.join()
   end
--- spec/leetspeak_spec.rb
+++ spec/leetspeak_spec.rb
@@ -12,10 +12,18 @@ describe('String#leetspeak') do
   end
 
   it('replaces the letter "o" with "0"') do
-    expect(("Opossum").leetspeak()).to(eq("0p0ssum"))
+    expect(("bOO boo").leetspeak()).to(eq("b00 b00"))
   end
 
   it('replaces the letter "I" (but not "i") with "1"') do
     expect(("Init").leetspeak()).to(eq("1nit"))
+  end
+
+  it('replaces the letter "s" with "z"') do
+    expect(("roses").leetspeak()).to(eq("r0z3z"))
+  end
+
+  it('does not replace "s" with "z" if it is the first letter of a word') do
+    expect(("sassafrass").leetspeak()).to(eq("sazzafrazz"))
   end
 end
